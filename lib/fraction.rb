d13874dc2fe19f084f5b8cddb35f9cd5dc8201ac
alu0100779622 <alu0100779622@ull.edu.es>
2015-10-28 23:43:44 +0000

    Pruebas Unitarias y clase Fraction terminadas

--- lib/fraction.rb
+++ lib/fraction.rb
@@ -1,27 +1,49 @@
 class Fraction
     attr_reader :num, :denom
+    
     def initialize(n,d)
-        @num = n
-        @denom = d
+        x = gcd(n,d)
+        @num = n / x
+        @denom = d / x
+        
     end
     
     def to_s
         "#{num}/#{denom}"
     end
     
-    def +(other)
+    def + (other)
+        z=mcm(@denom, other.denom)
+        x=@num*z/@denom
+        y=other.num*z/other.denom
+        Fraction.new(x+y,z)
     end
     
     def -(other)
+        z=mcm(@denom, other.denom)
+        x=@num*z/@denom
+        y=other.num*z/other.denom
+        Fraction.new(x-y,z)
     end
     
     def *(other)
+        Fraction.new(@num*other.num,@denom*other.denom)
     end
 
     def /(other)
+        Fraction.new(@num*other.denom,@denom*other.num)
+    end
+
+    def gcd(u,v)
+        u, v = u.abs, v.abs
+        while v != 0
+            u, v = v, u % v
+        end
+    u
     end
 
     def mcm(x,y)
+        z = x/gcd(x,y) * y
 	end
 
 end
